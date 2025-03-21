583aa5c40ed1d580cb668ed754e4413663f5e31a
PrassV <prassannavenkat@gmail.com>
2025-03-21 08:47:17 +0530

    commit

--- custom_start.sh
+++ custom_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd Backend
 export PYTHONPATH=$PYTHONPATH:$PWD
-python -m uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-8000} --timeout-keep-alive=300 
+python3 -m uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-8000} --timeout-keep-alive=300 
